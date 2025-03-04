d19326204253cf8d13759fd85b9b84e4626d3d54
Dopezapha <officialnwaneridaniel@gmail.com>
2025-03-04 14:24:25 +0100

    Blazer bot

--- Bot/main.py
+++ Bot/main.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 # Initialize the exchange
 def initialize_exchange():
-    exchange = ccxt.binance({
+    exchange = ccxt.bitget({
         'apiKey': os.getenv('bg_e83ddb4bf843a20f6627e844cd879f97'),
         'secret': os.getenv('10abc6d455b30c0dece31fce6c49642b55314200b67f7a845ba0f942c4137292'),
         'enableRateLimit': True,
